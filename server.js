521147b3a852896e5faee626dcc0bd2ce6cd7cb8
plxShow <plxshow@yahoo.com>
2024-09-12 13:43:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.post('/update-link', (req, res) => {
     const newLink = req.body.link;
     const key = req.body.key; // المفتاح الذي تريد تحديثه في admin.json
 
-    fs.readFile('admin.json', 'utf8', (err, data) => {
+    fs.readFile('https://8mdhi.netlify.app/admin.json', 'utf8', (err, data) => {
         if (err) {
             return res.status(500).send('حدث خطأ في قراءة الملف.');
         }
